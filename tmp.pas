142be77daafa35eba38c3be0de6ab5719a1aad36
ikatuli <84373378+ikatuli@users.noreply.github.com>
2021-06-15 00:12:22 +0700

    Добавленно readme и тестовая прога на паскале для тестирования.

---
+++ tmp.pas
@@ -0,0 +1,10 @@
+var bo,bo1:BOOLEAN;w:WORD;i:integer;r:REAL;c:CHAR;s:STRING;
+begin
+bo:=true;
+bo1:=False;
+w:=65535;
+i:=-100;
+c:='c';
+s:='STRING';
+writeln('bo= ',bo,' bo1= ',bo1,' w= ',w,' i= ',i,' c= ',c,' s= ',s);
+end.
